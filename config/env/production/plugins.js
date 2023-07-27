50d7312471e3b6207cf6a062c099d7109538805d
QMHLO <htetlinoo@qualymm.com>
2023-07-27 14:05:55 +0630

    email configured

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,5 +1,26 @@
 module.exports = ({ env }) => ({
   // ...
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        secure: true,
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "htetlinoo.qm@gmail.com",
+        defaultReplyTo: "htetlinoo.qm@gmail.com",
+      },
+    },
+  },
+  // ...
+  // ...
   upload: {
     config: {
       provider: "cloudinary",
